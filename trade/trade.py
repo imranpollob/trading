bd2802ac65cbbdd04cf42b271f7af807e1563e4e
imranpollob <polboy777@gmail.com>
2025-01-02 22:56:47 -0500

    use variable precision format

--- trade/trade.py
+++ trade/trade.py
@@ -5,6 +5,13 @@ def trade_summary(
     buy_fee_rate=0.0025,
     sell_fee_rate=0.0025,
 ):
+    default_format = "{:.4f}"
+    # if lenght of the coin_price after precision is more than 4, use the length of the input's precision
+    input_format = len(str(coin_price).split(".")[1])
+
+    if input_format > 4:
+        default_format = f"{{:.{input_format}f}}"
+
     # Calculate the number of coins bought
     num_coins = investment_amount / coin_price
 
@@ -15,10 +22,10 @@ def trade_summary(
     break_even_price = (total_cost / num_coins) / (1 - sell_fee_rate)
 
     # Output initial details
-    print(f"Price: ${coin_price:.6f}")
-    print(f"Paid:  ${investment_amount:.6f}")
+    print(f"Price: ${default_format.format(coin_price)}")
+    print(f"Paid:  ${default_format.format(investment_amount)}")
     print(f"Fee:   Buy {buy_fee_rate*100:.2f}% Sell {sell_fee_rate*100:.2f}%")
-    print(f"Break: ${break_even_price:.6f}\n")
+    print(f"Break: ${default_format.format(break_even_price)}\n")
 
     # If current coin price is provided, calculate profit/loss
     if current_coin_price is not None:
@@ -26,14 +33,16 @@ def trade_summary(
         net_profit_loss = sell_revenue - total_cost
         profit_loss_percentage = (net_profit_loss / total_cost) * 100
         print(
-            f"{profit_loss_percentage:.2f}%: ${current_coin_price:.6f} (${net_profit_loss:.2f})"
+            f"{profit_loss_percentage:.2f}%: ${default_format.format(current_coin_price)} (${net_profit_loss:.2f})"
         )
 
     # Target prices and profit amounts for 1% to 5% profit
     for i, profit in enumerate([0.01, 0.02, 0.03, 0.04, 0.05], 1):
         price = break_even_price * (1 + profit)
         profit_amount = (price - break_even_price) * num_coins * (1 - sell_fee_rate)
-        print(f"{profit * 100:.2f}%: ${price:.6f} (${profit_amount:.2f})")
+        print(
+            f"{profit * 100:.2f}%: ${default_format.format(price)} (${profit_amount:.2f})"
+        )
 
 
 def main():
