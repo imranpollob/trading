03ce3ec76f42c298f255dd13eacdc86016d7275f
imranpollob <polboy777@gmail.com>
2025-01-05 18:43:05 -0500

    added loss targets

--- trade/trade.py
+++ trade/trade.py
@@ -36,12 +36,22 @@ def trade_summary(
             f"{profit_loss_percentage:.2f}%: ${default_format.format(current_coin_price)} (${net_profit_loss:.2f})\n"
         )
 
-    # Target prices and profit amounts for 1% to 5% profit
+    # Profit targets
     for i, profit in enumerate([0.005, 0.01, 0.02, 0.03, 0.04, 0.05], 1):
         price = break_even_price * (1 + profit)
         profit_amount = (price - break_even_price) * num_coins * (1 - sell_fee_rate)
         print(
             f"{profit * 100:.2f}%: ${default_format.format(price)} (${profit_amount:.2f})"
+        )
+
+    print()
+
+    # Losing targets
+    for i, loss in enumerate([0.005, 0.01, 0.02, 0.03, 0.04, 0.05], 1):
+        price = break_even_price * (1 - loss)
+        loss_amount = (break_even_price - price) * num_coins * (1 - sell_fee_rate)
+        print(
+            f"-{loss * 100:.2f}%: ${default_format.format(price)} (-${loss_amount:.2f})"
         )
 
 
